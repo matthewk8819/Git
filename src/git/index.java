30638326d1623e871dba879357f0af7a717911d4
Matthew <matthewk8819@gmail.com>
2022-09-01 15:56:04 -0700

    Yes

---
+++ src/git/index.java
@@ -0,0 +1,17 @@
+package git;
+import java.util.*;
+
+
+public class index {
+	
+	
+	public index() {
+		System.out.println("HELLO!");
+		//bro
+	}
+	
+	//makes an object folder, index file 
+	public void init() {
+		//hello
+	}
+}
