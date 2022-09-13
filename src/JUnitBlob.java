f78fb0d72a9bce4363b7d855177f676b5d786eda
Naalah Cohen <naalah28@gmail.com>
2022-09-13 11:13:45 -0700

    added to check if the name of the hash function is correct

--- src/JUnitBlob.java
+++ src/JUnitBlob.java
@@ -1,10 +1,12 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -12,25 +14,19 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 import git.Blob;
 
 class JUnitBlob {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		Path p = Paths.get("test.txt");
-        try {
-            Files.writeString(p, "example", StandardCharsets.ISO_8859_1);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
+		FileUtil.writeStringToFile("test.txt");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
-		deleteFile("test.txt");
+		FileUtil.deleteStringToFile("test.txt");
 	}
 
 	@BeforeEach
@@ -45,7 +41,14 @@ class JUnitBlob {
 	@Test
 	void test() throws IOException {
 		Blob bnew = new Blob ("test.txt"); 
-		
+		String sha1 = bnew.getSha1("test.txt");
+		assertTrue(sha1.equals("adc83b19e793491b1c6ea0fd8b46cd9f32e592fc"));
 	}
+	
+	
+	
+
+	
+	
 
 }
