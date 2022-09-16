a448437afc07e5c49e06ea1016ce806167ea4d2f
Naalah Cohen <naalah28@gmail.com>
2022-09-16 00:44:57 -0700

    added in sha1 method, working on generating all

--- src/Tree.java
+++ src/Tree.java
@@ -1,4 +1,32 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.ArrayList;
 
 public class Tree {
+	public Tree (ArrayList <String> arr) {
+		
+	}
+	
+	public String generateAll (ArrayList <String> arr) {
+		String str = ""; 
+		for(int i =0; i < arr.size(); i++) {
+			str += arr.get(i);
+		}
+		
+	}
+	
+	public String sha1 (String input) {
+		String value = input;
+		String sha1 = "";
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-1");
+	        digest.reset();
+	        digest.update(value.getBytes("utf8"));
+	        sha1 = String.format("%040x", new BigInteger(1, digest.digest()));
+		} catch (Exception e){
+			e.printStackTrace();
+		}
 
+		return sha1;
+	}
 }
